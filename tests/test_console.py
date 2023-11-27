bc060723f784e26f8b2e0543122d2ef1596dbdfb
Kevin.P <kevin.pellissard@gmail.com>
2023-11-28 00:54:43 +0100

    update task and pycodestyle

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 from io import StringIO
 import os
 from console import HBNBCommand
+import pycodestyle
 
 
 class TestConsole(unittest.TestCase):
@@ -27,7 +28,6 @@ class TestConsole(unittest.TestCase):
         except Exception:
             pass
 
-
     def test_create(self):
         """Test create command inpout"""
         with patch('sys.stdout', new=StringIO()) as f:
@@ -44,3 +44,24 @@ class TestConsole(unittest.TestCase):
             self.consol.onecmd("all User")
             self.assertEqual(
                 "[[User]", f.getvalue()[:7])
+
+    def test_doc_console(self):
+        self.assertIsNotNone(HBNBCommand.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_count.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
+        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
+
+    def testPycodeStyle(self):
+        """Test pycodestyle for console"""
+        style = pycodestyle.StyleGuide(quiet=True)
+        p = style.check_files(['console.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+
+if __name__ == '__main__':
+    unittest.main()
