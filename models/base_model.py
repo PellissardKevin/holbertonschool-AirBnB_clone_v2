7b7708eeb093f5b704e7d92c81abf5d3c478c8ed
Kevin.P <kevin.pellissard@gmail.com>
2023-11-28 16:56:59 +0100

    Update base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from models.storage import FileStorage
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Column, Integer, DateTime
+
+Base = declarative_base()
 
 
-class BaseModel:
+class BaseModel(Base):
     """A base class for all hbnb models"""
 
     def __init__(self, *args, **kwargs):
@@ -12,11 +17,15 @@ class BaseModel:
         if not kwargs:
             from models import storage
 
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            id = Column(String(60), nullable=False, primary_key=True)
+            # self.id = str(uuid.uuid4())
+            created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+            # self.created_at = datetime.now()
+            updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+            # self.updated_at = datetime.now()
         else:
+            for key, value in kwargs:
+                self.key = value
             kwargs["updated_at"] = datetime.strptime(
                 kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
             )
@@ -36,13 +45,21 @@ class BaseModel:
         from models import storage
 
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
+        if self.__dict__["_sa_instance_state"]:
+            del self.__dict__["_sa_instance_state"]
+
         dictionary.update({"__class__": (str(type(self)).split(".")[-1]).split("'")[0]})
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
+
+        def delete(self):
+            FileStorage().delete(self)
+            FileStorage().save()
