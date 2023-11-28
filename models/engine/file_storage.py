9469e1ed50ed56086a8d72626609c6fb9dc2ab1f
ER1-53 <cremey.erwan@gmail.com>
2023-11-28 14:25:08 +0100

    task 5 modif strorage file all & delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,9 +9,12 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -53,3 +56,8 @@ class FileStorage:
                     self.all()[key] = classes[val["__class__"]](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects.pop(key, None)
