fb4be14333e72bff245bcc7172813c4c1d224706
ER1-53 <cremey.erwan@gmail.com>
2023-11-30 16:08:45 +0100

    tack 10

--- models/amenity.py
+++ models/amenity.py
@@ -9,4 +9,4 @@ class Amenity(BaseModel, Base):
     """Amenity class"""
 
     __tablename__ = 'amenities'
-    name = "" """Column(String(128), nullable=False)"""
+    name = Column(String(128), nullable=False)
