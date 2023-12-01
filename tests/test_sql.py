ed949961e75bcc88f68477350ee9bd0052d765ee
Kevin.P <kevin.pellissard@gmail.com>
2023-12-01 23:06:09 +0100

    Update test sql with every class

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -57,6 +57,68 @@ class TestMySQL(unittest.TestCase):
         self.assertEqual(res, 1)
         self.disconnection()
 
+    def test_create_user(self):
+        """Test create of a User"""
+        self.connection()
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create User firstname="John"')
+        self.cur.execute("SELECT COUNT(*) FROM users")
+        res = self.cur.fetchone()[0]
+        self.assertEqual(res, 1)
+        self.disconnection()
+
+    def test_create_place(self):
+        """Test create of a place"""
+        self.connection()
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create State name="California"')
+        id_state = f.getvalue()[:-1]
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd(f'''create City state_id="{id_state}"
+                                 name="San_Francisco"''')
+        id_city = f.getvalue()[:-1]
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd(f'''create Place city_id="{id_city}"
+                                 name="Lovely_place"''')
+        self.cur.execute("SELECT COUNT(*) FROM places")
+        res = self.cur.fetchone()[0]
+        self.assertEqual(res, 1)
+        self.disconnection()
+
+    def test_create_review(self):
+        """Test create of a Review"""
+        self.connection()
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create State name="California"')
+        id_state = f.getvalue()[:-1]
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd(f'''create City state_id="{id_state}"
+                                 name="San_Francisco"''')
+        id_city = f.getvalue()[:-1]
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd(f'''create Place city_id="{id_city}"
+                                 name="Lovely_place"''')
+        id_place = f.getvalue()[:-1]
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create User firstname="John"')
+        id_user = f.getvalue()[:-1]
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd(f'''create Review place_id="{id_place}"
+                                 user_id="{id_user}" text="Charming"''')
+        self.cur.execute("SELECT COUNT(*) FROM reviews")
+        res = self.cur.fetchone()[0]
+        self.assertEqual(res, 1)
+        self.disconnection()
+
+    def test_create_amenity(self):
+        """Test create of a Amenity"""
+        self.connection()
+        with patch('sys.stdout', new=io.StringIO()) as f:
+            HBNBCommand().onecmd('create Amenity name="Wifi"')
+        self.cur.execute("SELECT COUNT(*) FROM amenities")
+        res = self.cur.fetchone()[0]
+        self.assertEqual(res, 1)
+        self.disconnection()
 
 if __name__ == '__main__':
     unittest.main()
