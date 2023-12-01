6cdd1eeeb0a399766fc2de833616a3a3beb6ed3d
Kevin.P <kevin.pellissard@gmail.com>
2023-12-01 22:39:50 +0100

    Add some test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -51,7 +51,7 @@ class TestDBstorage(unittest.TestCase):
         self.assertIsNotNone(DBStorage.reload.__doc__)
 
     def testPycodeStyle(self):
-       style = pycodestyle.StyleGuide(quiet=True)
+        style = pycodestyle.StyleGuide(quiet=True)
         p = style.check_files(['models/engin/db_storage.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
